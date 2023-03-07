d5c211d68c909046334a05787924ae0e94bffa19
AlexNompe <Alex.Nompe@yandex.ru>
2023-03-07 21:37:01 +0300

    (Alex) Player controller upgraded + sunglases(eyes) cube with material added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float camScroll;
 
     private Vector2 moveInput;
     private Vector2 moveVelocity;
+    private float camInput = 0;
+    public float camVelocity;
 
     private Rigidbody rb;
+    private GameObject cam;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cam = transform.Find("Main Camera").gameObject;
     }
 
     private void FixedUpdate()
@@ -20,11 +25,31 @@ public class PlayerController : MonoBehaviour
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
 
-        rb.transform.position += new Vector3(moveVelocity.x, 0, moveVelocity.y) * Time.deltaTime;
+        rb.transform.position += rb.transform.right * (Greater0(moveVelocity.x) * Time.deltaTime) + rb.transform.right * (Lesser0(moveVelocity.x) * Time.deltaTime) + rb.transform.forward * (Greater0(moveVelocity.y) * Time.deltaTime) + rb.transform.forward * (Lesser0(moveVelocity.y) * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Q)) camInput = -1;
+        else if (Input.GetKey(KeyCode.E)) camInput = 1;
+        else camInput = 0;
+
+        camVelocity = camInput * camScroll;
+
+        rb.transform.Rotate(new Vector3(0, 1, 0) * camVelocity * Time.deltaTime);
     }
 
     void Update()
     {
         
+    }
+
+    public float Greater0(float i)
+    {
+        if(i > 0) return i;
+        else return 0;
+    }
+
+    public float Lesser0(float i)
+    {
+        if (i < 0) return i;
+        else return 0;
     }
 }
